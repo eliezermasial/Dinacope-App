27171600915f49b6284461f7f46da3a1ae80a876
eliezermasial <eliezermasiala2000@gmail.com>
2024-09-23 19:38:40 +0200

    modification of the school file

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('layouts.tables.tableEcole');
+    return view('layouts.tables.ecole');
 });
 
 Route::get('/eleve', [\App\Http\Controllers\EleveServiceController::class, 'getEleve']);
